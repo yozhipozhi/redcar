0d981eb526a728ffebeaa5429239b5c5a728cc36
Tom Murray <yp@google.com>
2011-01-30 00:28:27 -0800

    Added support for :style UI hints, to be set in the widget constructor.

--- plugins/application_swt/lib/application_swt/speedbar/label_item.rb
+++ plugins/application_swt/lib/application_swt/speedbar/label_item.rb
@@ -3,12 +3,19 @@ module Redcar
     class Speedbar
       class LabelItem
         def initialize(speedbar, composite, item)
-          label = Swt::Widgets::Label.new(composite, 0)
+          ui_hints = UIHints.get_item_hints(item)
+          style = 0
+          if ui_hints && (ui_hints.has_key? :style)
+            style = UIHints.get_hint_value(nil, :style, ui_hints[:style])
+            ui_hints = ui_hints.dup
+            ui_hints.delete(:style)
+          end
+          label = Swt::Widgets::Label.new(composite, style)
           label.set_text(item.text)
           item.add_listener(:changed_text) do |new_text|
             label.set_text(item.text)
           end
-          UIHints.apply_hints(label, item)
+          UIHints.apply_hints(label, ui_hints)
         end
       end
     end
